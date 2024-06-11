1fe3d9f262d04d092c5c6d267581d85d4d79859d
[Nawaz453] <[md453shahnawaz@gmail.com]>
2024-06-11 14:51:38 +0530

    fixed logo div on small screen-moblie

--- script.js
+++ script.js
@@ -1,33 +1,72 @@
-//nav animation
-{
-let t1=gsap.timeline();
-t1.from("#logo",{
-    x:-500,
-    scale:2,
-    duration:1
-})
-t1.to("#logo",{
-    x:-100,
-    duration:0.7,
-    delay:0
-})
-t1.from("#logo h1",{
-    scale:0,
-    y:-100
-})
-let t2=gsap.timeline();
-t2.from("#img",{
-    x:500,
-    scale:2,
-    duration:1
-})
-t2.to("#img",{
-    x:100,
-    duration:0.7,
-    delay:0
-})
-t2.from("#logo p",{
-    scale:0,
-    y:100
-})
+//nav animation 
+function mediaquary(x){
+    if(x.matches){
+        let t1=gsap.timeline();
+        t1.from("#logo",{
+            x:-500,
+            scale:2,
+            duration:1
+        })
+        t1.to("#logo",{
+            x:-30,
+            duration:0.7,
+            delay:0
+        })
+        t1.from("#logo h1",{
+            scale:0,
+            y:-100
+        })
+        let t2=gsap.timeline();
+        t2.from("#img",{
+            x:500,
+            scale:2,
+            duration:1
+        })
+        t2.to("#img",{
+            x:30,
+            duration:0.7,
+            delay:0
+        })
+        t2.from("#logo p",{
+            scale:0,
+            y:100
+        })
+    }
+    else{
+        let t1=gsap.timeline();
+        t1.from("#logo",{
+            x:-500,
+            scale:2,
+            duration:1
+        })
+        t1.to("#logo",{
+            x:-100,
+            duration:0.7,
+            delay:0
+        })
+        t1.from("#logo h1",{
+            scale:0,
+            y:-100
+        })
+        let t2=gsap.timeline();
+        t2.from("#img",{
+            x:500,
+            scale:2,
+            duration:1
+        })
+        t2.to("#img",{
+            x:100,
+            duration:0.7,
+            delay:0
+        })
+        t2.from("#logo p",{
+            scale:0,
+            y:100
+        })
+    }
 }
+var x=window.matchMedia("(max-width:750px)")
+mediaquary(x);
+x.addEventListener("change",function(){
+    mediaquary(x);
+});
